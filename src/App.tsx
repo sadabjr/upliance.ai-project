148fb92d96032df5cb3f9e06067730d55877e1a7
Sadab Ali <sadabjr047@gmail.com>
2025-02-05 00:01:45 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
-import AuthPage from "./pages/AuthPage";
 import { SignedIn, SignedOut } from "@clerk/react-router";
 import Dashboard from "./pages/Dashboard";
+import AuthPage from "./pages/AuthPage.tsx";
 
 function App() {
   return (
